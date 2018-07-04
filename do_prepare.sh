c2f5930568d96a12124f8cc1a8cbf3d8bca27fbf
xuchaoxi <xuchaoxikb@163.com>
2018-07-04 11:29:50 +0800

    download inception-v3 pre-trained model

--- do_prepare.sh
+++ do_prepare.sh
@@ -22,6 +22,15 @@ elif [ $1 = '11k' ]; then
 
     wget http://data.mxnet.io/models/imagenet-11k/resnet-152/resnet-152-symbol.json
     wget http://data.mxnet.io/models/imagenet-11k/resnet-152/resnet-152-0000.params
+elif [ $1 = 'v3' ]; then
+    mkdir -p $HOME/premodels/yt8m
+    cd $HOME/premodels/yt8m
+
+    wget http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz
+    wget http://data.yt8m.org/yt8m_pca.tgz
+
+    tar zxvf inception-2015-12-05.tgz
+    tar zxvf yt8m_pca.tgz
 
 else
     echo "data_type is invalid"
