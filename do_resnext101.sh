20287964b99a14168d8de68a35d6f8a441707e19
xuchaoxi <7885939+xuchaoxi@users.noreply.github.com>
2019-11-20 11:18:21 +0800

    Update do_resnext101.sh

--- do_resnext101.sh
+++ do_resnext101.sh
@@ -1,24 +1,19 @@
-gpu_id=0  # set to -1 if gpu is not available
-rootpath=$HOME/VisualSearch
-oversample=1
-overwrite=0
-
-raw_feat_name=pyresnext-101_rbps13k,flatten0_output
-
-imgpath_file=id.imagepath.txt
+source common.ini
 
 if [ "$#" -lt 1 ]; then
     echo "Usage: $0 test_collection [rootpath]"
     exit
 fi
 
-test_collection=$1
-
-model_prefix=mxmodels80/resnext-101_rbps13k_step_3_aug_1_dist_3x2/resnext-101-1-0040
-
 if [ "$#" -gt 1 ]; then
     rootpath=$2
 fi
+
+test_collection=$1
+
+model_prefix=mxmodels80/resnext-101_rbps13k_step_3_aug_1_dist_3x2/resnext-101-1-0040
+raw_feat_name=pyresnext-101_rbps13k,flatten0_output
+
 
 ./do_deep_feat.sh ${gpu_id} ${rootpath} ${oversample} ${overwrite} ${raw_feat_name} ${test_collection} ${model_prefix}
 
