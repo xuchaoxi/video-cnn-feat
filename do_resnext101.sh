fb14d76dec99d028e965f85e765d36bcd8ffded7
xuchaoxi <7885939+xuchaoxi@users.noreply.github.com>
2019-11-20 11:15:53 +0800

    Update do_resnext101.sh

--- do_resnext101.sh
+++ do_resnext101.sh
@@ -15,11 +15,10 @@ fi
 test_collection=$1
 
 model_prefix=mxmodels80/resnext-101_rbps13k_step_3_aug_1_dist_3x2/resnext-101-1-0040
-mxmodel_dir=$rootpath/mxmodels80/resnext-101_rbps13k_step_3_aug_1_dist_3x2
 
 if [ "$#" -gt 1 ]; then
     rootpath=$2
 fi
 
-./do_deep_feat.sh ${gpu_id} ${rootpath} ${oversample} ${overwrite} ${raw_feat_name} ${test_collection} ${model_prefix} ${mxmodel_dir} ${imgpath_file}
+./do_deep_feat.sh ${gpu_id} ${rootpath} ${oversample} ${overwrite} ${raw_feat_name} ${test_collection} ${model_prefix}
 
